487f100b37733f1dea69a1d74bf521ee879565f6
CamilleDiez <39010959+CamilleDiez@users.noreply.github.com>
2018-05-05 19:14:04 +0200

    Add files via upload

---
+++ ProgMargotCamille.py
@@ -0,0 +1,149 @@
+#! /Applications/Anaconda/envs/naima/bin/python
+
+import astropy.units as u
+import naima as nai
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import integrate, special
+from math import *
+#from lmfit import minimize, Parameters
+
+N1=50
+N2=50
+N3=15
+
+eV2erg=1.6022e-12  # PM
+pc2cm=3.086e18  # PM
+u_mag=(3e-6)**2.0/8.0/pi     # erg/cm3 (for 3microGauss)
+u_rad=0.26*eV2erg            # erg/cm3 (for CMB at 2.7K)  # PM
+eps_rad=4.55e-10             # in m_e*c^2 (for CMB at 2.7K)
+c=3e10                       # en cm/s
+sigma_T=6.65e-25             # en cm2
+mec2=511e3*eV2erg            # en erg  # PM
+
+
+#D100=4.5*10**(27)/((3.086*10**(18))**2) #pc2/s
+D100=4.5e27/pc2cm**2 #pc2/s  # PM
+delta=1./3.
+
+dsrc=250 #parsecs
+#N0=13.6*10**(-15) #photons/TeV/cm2/s
+#alpha=2.34
+eta=0.3   #efficacite, on la prend a 0.1 ou 0.4
+L=3.26E34/eV2erg*1e-12    #energy loss rate, voir tableau article  # PM TeV/s
+gamma=2.15
+Q0=(eta*L*(-gamma+2))/(5e2**(-gamma+2)-1e-3**(-gamma+2))  # PM TeV^(gamma-1)/s
+dmax=100
+
+age_plsr=342000*365.25*24*3600 #en sec
+
+Ee=np.logspace(log(0.1,10),log(500,10),N1) #energie des electrons
+Eg=np.logspace(log(0.1,10),log(100,10),N2)  #energie des photons
+deltad=dmax/N3
+d=np.linspace(0,dmax-deltad,N3-1)+deltad/2    #distance au centre de la source
+
+
+
+#theta=d/(pi*(dsrc/180))  # PM (cette variable ne sert pas apparemment)
+
+ILV=[]    #integrale sur la ligne de visee
+SB=[]     #surface brightness
+SB2=[]     #surface brightness PM, test of a new integration scheme
+
+def log_integral(x,y):
+	res=0.0
+	if x.size != y.size:
+		print ('Incompatible arrays for log-integral calculation.')
+	else:
+		for i in range(1,x.size):
+			res += (x[i]-x[i-1])/(x[i]+x[i-1])*(x[i]*y[i]+x[i-1]*y[i-1])
+	return res
+
+
+def M(l,d,D,rd):
+    #print(np.array(D))
+    return (Q0*special.erfc(sqrt(l**2+d**2)/rd))/(sqrt(l**2+d**2)*4*pi*D)  # PM TeV^(gamma-1)/pc3
+
+
+        
+#print(np.array(D)) 
+
+for i in d:
+    s=[]
+    rayon=[]
+    for j in Ee:
+        D=D100*pow((j/1e2),delta)   # PM pc2/s
+        #boucle pour choisir le temps le plus restrictif
+        #lorentz=j/mec2  # PM j is in TeV and mec2 is in erg !!! Difference by a factor 1.6
+        lorentz=j/0.511e-6 
+        tcool=mec2/((4./3)*c*sigma_T*lorentz*(u_mag+u_rad/(1+4*lorentz*eps_rad)**1.5))   # PM s
+        if tcool>=age_plsr:
+            tcool = age_plsr
+        rd=2*sqrt(D*tcool)  # PM pc
+        rayon.append(rd)
+        #m=integrate.quad(M,-np.inf,np.inf,args=(i,D,rd)) 
+        m=integrate.quad(M,-dsrc,dsrc,args=(i,D,rd))  # PM TeV^(gamma-1)/pc2
+        s.append(m[0])
+    #thetamin=np.arccos((i-deltad/2)/dmax)
+    #thetamax=np.arccos((i+deltad/2)/dmax)
+    #surface=2*np.pi*(dmax**2)*(np.cos(thetamax)-np.cos(thetamin))  ##
+    #s*=Ee**(-gamma)*(dsrc**2)*u.Unit('1/TeV')
+    s=np.asarray(s)*np.power(Ee,-gamma)*(dsrc**2)*(pi/180.)**2.0  # PM /TeV/deg2
+    ILV.append(s)
+    TB=nai.models.TableModel(Ee*u.TeV, s/u.TeV, amplitude=1)  # PM
+    IC = nai.models.InverseCompton(TB, seed_photon_fields=['CMB'])
+    flux_IC = IC.flux(Eg*u.TeV, distance=dsrc*u.pc)  #mettre IC.flux ou IC.sed en fct de ce que l'on veut
+    flux_IC = flux_IC.to(u.TeV**-1*u.cm**-2*u.s**-1)
+    fig1 = plt.figure(1)
+    plt.loglog(Eg,Eg*Eg*flux_IC.value)  #rajouter Eg*Eg*flux si on veut sed  # PM TeV/cm2/s
+    plt.xlabel("Photons energy [TeV]")
+    plt.ylabel("Spectral energy density (SED) [TeV/cm2/s]")
+    idx=(Eg>=5) & (Eg<=50) # PM pour eviter de faire 3x la meme operation
+    #sb=integrate.cumtrapz(Eg[idx]*flux_IC.value[idx],Eg[idx]) # PM
+    #SB.append(sb[0])
+    sb2=log_integral(Eg[idx],Eg[idx]*flux_IC.value[idx])
+    SB2.append(sb2*1E12)  #on normalise avec 10^12
+    fig5 = plt.figure(5)
+    plt.loglog(Ee,rayon) 
+    plt.xlabel("Electrons energy [TeV]")
+    plt.ylabel("Distance from pulsar [pc]")
+
+# Test integration schemes on power laws
+a=2.0
+p=10.0
+x=np.logspace(0,3,60)
+y=p*np.power(x,-a)
+int1=integrate.cumtrapz(y,x) # Original method
+int2=log_integral(x,y)       # Log method
+int3=p*(x[-1]**(1-a)-x[0]**(1-a))/(1-a)
+print (int1[-1],int2,int3)
+
+fig2 = plt.figure(2)
+
+for k in range(len(d)):
+    plt.loglog(Ee,ILV[k])  
+plt.xlabel("Energie des électrons [TeV]")
+plt.ylabel("Integrale sur la ligne de visee [électrons/TeV/deg2]")  #flux d'electrons # PM pas la bonne unite ici, ce sont des electrons/TeV/deg2
+
+
+fig3 = plt.figure(3)
+ax1 = fig3.add_subplot(111)
+
+plt.plot(d,SB2,'r-')   
+#plt.plot(d,SB,'b-')   
+ax1.set_xlim(0,50)  
+ax1.set_xlabel('Distance from pulsar [pc]')
+ax1.set_ylabel('Surface brightness [10^-12 TeV/cm2/s/deg2]')
+ax2 = ax1.twiny()  
+ax2.set_xlim(0,12) 
+ax2.set_xlabel('Distance from pulsar [degree]')
+
+plt.show()
+
+#import matplotlib.cm as cm
+#colorlist = cm.rainbow(N2) # N étant le nombre de courbes à afficher
+#for a in range(d):
+#    color=colorlist[a]
+#    y=Eg*Eg*flux_IC[a]
+##plt.plot(x,y,color=color)
+#plt.loglog(Eg,y,color=color)
